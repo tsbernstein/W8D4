ba7f8b6cd7048739c29f05cdb588d9ca77067d9b
joseph <josephwyang@gmail.com>
2021-06-10 16:20:01 -0400

    completed myThrottle

--- callback_exercises/callback_exercises.js
+++ callback_exercises/callback_exercises.js
@@ -165,10 +165,66 @@ const turnOn = function() {
 
 const lamp = new Lamp();
 
-turnOn(); // should not work the way we want it to
+// turnOn(); // should not work the way we want it to
 
 const boundTurnOn = turnOn.bind(lamp);
 const myBoundTurnOn = turnOn.myBind(lamp);
 
-boundTurnOn(); // should say "Turning on a lamp"
-myBoundTurnOn(); // should say "Turning on a lamp"
+// boundTurnOn(); // should say "Turning on a lamp"
+// myBoundTurnOn(); // should say "Turning on a lamp"
+
+
+// myThrottle
+// class Neuron {
+//   fire() {
+//     console.log("Firing!");
+//   }
+// }
+
+// const neuron = new Neuron();
+// // When we create a new Neuron,
+// // we can call #fire as frequently as we want
+
+// // The following code will try to #fire the neuron every 10ms. Try it in the console:
+// const interval = setInterval(() => {
+//   neuron.fire();
+// }, 10);
+
+// // You can use clearInterval to stop the firing:
+// clearInterval(interval);
+
+// This time, if our Function#myThrottle worked correctly,
+// the Neuron#fire function should only be able to execute
+// every 500ms, even though we're still trying to invoke it
+// every 10ms!
+
+// If we want this behavior for ALL neurons, we can do the same logic in the constructor:
+
+Function.prototype.myThrottle = function(interval) {
+  let tooSoon = false;
+  return () => {
+    if(!tooSoon) {
+      tooSoon = true;
+      setTimeout(() => { tooSoon = false; }, interval);
+      this();
+    }
+  }
+}
+
+class Neuron {
+  constructor() {
+    this.fire = this.fire.myThrottle(500);
+  }
+
+  fire() {
+    console.log("Firing!");
+  }
+}
+
+const neuron = new Neuron();
+neuron.fire = neuron.fire.myThrottle(500);
+
+const interval = setInterval(() => {
+  neuron.fire();
+}, 10);
+
